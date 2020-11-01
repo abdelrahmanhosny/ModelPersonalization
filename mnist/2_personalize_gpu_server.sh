71a0b34d463c33b61e35929ed6d3df76013abb36
Abdelrahman <abdelrahman@brown.edu>
2020-11-01 03:35:13 -0500

    add sleep to get idle power

--- mnist/2_personalize_gpu_server.sh
+++ mnist/2_personalize_gpu_server.sh
@@ -6,6 +6,10 @@ BATCH_SIZE=${2:-64}
 nvidia-smi --query-gpu=timestamp,pstate,utilization.gpu,utilization.memory,temperature.gpu,temperature.memory,power.draw --format=csv -l 1 -f data/QMNIST/models-subject-out/$WRITER_ID/personalize-server-gpu-$BATCH_SIZE.csv &
 pid=$!
 
-python mnist/2_personalize.py --writer_id=$WRITER_ID --batch_size=$BATCH_SIZE |& tee data/QMNIST/models-subject-out/$WRITER_ID/personalize-server-gpu-$BATCH_SIZE.log
+sleep 5        # used to report idle power when util is zero
+
+python mnist/2_personalize.py --log-interval=1 --writer_id=$WRITER_ID --batch_size=$BATCH_SIZE |& tee data/QMNIST/models-subject-out/$WRITER_ID/personalize-server-gpu-$BATCH_SIZE.log
+
+sleep 5        # used to report idle power when util is zero
 
 kill $pid
