95ec3d77473db6d6801bdc6245569a7e77420af0
Abdelrahman <abdelrahman@brown.edu>
2020-11-01 18:19:53 -0500

    fix sample counting

--- mnist/4_report.py
+++ mnist/4_report.py
@@ -24,7 +24,8 @@ if __name__ == "__main__":
         for writer_id in range(131):
             with open(os.path.join('data', 'QMNIST', 'models-subject-out', str(writer_id), 'personalize-server-gpu-' + str(batch_size) + '.csv'), 'r') as f:
                 _ = f.readline() # discard header
-                samples = 0
+                active_samples = 0
+                idle_samples = 0
                 writer_idle_power = 0
                 writer_active_power = 0
                 writer_gpu_util = 0
@@ -32,22 +33,23 @@ if __name__ == "__main__":
                 writer_temp = 0
 
                 for sample in f:
-                    samples += 1
                     ts, pstate, util_gpu, util_mem, temp_gpu, temp_mem, power = sample.strip().split(',')
-                    if util_gpu.strip() == '0 %' and util_mem == '0 %'.strip():
+                    if util_gpu.strip() == '0 %' and util_mem.strip() == '0 %':
                         # idle state
                         writer_idle_power += float(power.strip().split(' ')[0])
+                        idle_samples += 1
                     else:
                         writer_active_power += float(power.strip().split(' ')[0])
                         writer_gpu_util += float(util_gpu.strip().split(' ')[0])
                         writer_mem_util += float(util_mem.strip().split(' ')[0])
                         writer_temp += float(temp_gpu.strip())
+                        active_samples += 1
                 
-                gpu_utils.append(writer_gpu_util / samples)
-                mem_utils.append(writer_mem_util / samples)
-                active_pwr.append(writer_active_power / samples)
-                idle_pwr.append(writer_idle_power / samples)
-                temp.append(writer_temp / samples)
+                gpu_utils.append(writer_gpu_util / active_samples)
+                mem_utils.append(writer_mem_util / active_samples)
+                active_pwr.append(writer_active_power / active_samples)
+                idle_pwr.append(writer_idle_power / idle_samples)
+                temp.append(writer_temp / active_samples)
         print(batch_size, sum(gpu_utils) / len(gpu_utils), \
             sum(mem_utils) / len(mem_utils), \
                 sum(temp) / len(temp), \
