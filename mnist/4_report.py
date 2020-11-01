4eac14f579a43b9328e6343d51b4ddce623eb73c
Abdelrahman <abdelrahman@brown.edu>
2020-11-01 18:02:40 -0500

    reporting fixes

--- mnist/4_report.py
+++ mnist/4_report.py
@@ -1,3 +1,4 @@
+import os
 import torch
 
 from collections import defaultdict
@@ -6,9 +7,9 @@ from edgify.utils import WriterQMNIST
 
 if __name__ == "__main__":
     for writer_id in range(131):
-        with open(os.path.join('data', 'QMNIST', 'model-subjet-out', str(writer_id), 'accuracy_before.txt'), 'r') as f:
+        with open(os.path.join('data', 'QMNIST', 'model-subject-out', str(writer_id), 'accuracy_before.txt'), 'r') as f:
             accuracy_before = f.readline().strip()
-        with open(os.path.join('data', 'QMNIST', 'model-subjet-out', str(writer_id), 'accuracy_after.txt'), 'r') as f:
+        with open(os.path.join('data', 'QMNIST', 'model-subject-out', str(writer_id), 'accuracy_after.txt'), 'r') as f:
             accuracy_after = f.readline().strip()
         print(accuracy_before, accuracy_after)
         
@@ -21,7 +22,7 @@ if __name__ == "__main__":
         idle_pwr = []
         temp = []
         for writer_id in range(131):
-            with open(os.path.join('data', 'QMNIST', 'model-subjet-out', str(writer_id), 'personalize-server-gpu-' + str(batch_size) + '.csv'), 'r') as f:
+            with open(os.path.join('data', 'QMNIST', 'models-subject-out', str(writer_id), 'personalize-server-gpu-' + str(batch_size) + '.csv'), 'r') as f:
                 _ = f.readline() # discard header
                 samples = 0
                 writer_idle_power = 0
@@ -35,18 +36,18 @@ if __name__ == "__main__":
                     ts, pstate, util_gpu, util_mem, temp_gpu, temp_mem, power = list(filter(lambda  x: x.strip(), sample.strip().split(',')))
                     if util_gpu == '0 %' and util_mem == '0 %':
                         # idle state
-                        writer_idle_power.append(float(power.split(' ')[0]))
+                        writer_idle_power += float(power.split(' ')[0])
                     else:
-                        writer_active_power.append(float(power.split(' ')[0]))
-                        writer_gpu_util.append(float(util_gpu.split(' ')[0]))
-                        writer_mem_util.append(float(util_mem.split(' ')[0]))
-                        writer_temp.append(float(temp_gpu))
+                        writer_active_power += float(power.split(' ')[0])
+                        writer_gpu_util += float(util_gpu.split(' ')[0])
+                        writer_mem_util += float(util_mem.split(' ')[0])
+                        writer_temp += float(temp_gpu)
                 
-                gpu_utils.append(sum(writer_gpu_util) / samples)
-                mem_utils.append(sum(writer_mem_util) / samples)
-                active_pwr.append(sum(writer_active_power) / samples)
-                idle_pwr.append(sum(writer_idle_power) / samples)
-                temp.append(sum(writer_temp) / samples)
+                gpu_utils.append(writer_gpu_util / samples)
+                mem_utils.append(writer_mem_util / samples)
+                active_pwr.append(writer_active_power / samples)
+                idle_pwr.append(writer_idle_power / samples)
+                temp.append(writer_temp / samples)
         print(batch_size, sum(gpu_utils) / len(gpu_utils), \
             sum(mem_utils) / len(mem_utils), \
                 sum(temp) / len(temp), \
