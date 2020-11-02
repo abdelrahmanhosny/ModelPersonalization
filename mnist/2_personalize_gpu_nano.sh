ccd564ac1d8a0790c38d982b0fd490a37ad2574d
Abdelrahman <abdelrahman@brown.edu>
2020-11-02 16:03:50 -0500

    reporting + training from scratch

--- mnist/2_personalize_gpu_nano.sh
+++ mnist/2_personalize_gpu_nano.sh
@@ -3,8 +3,8 @@
 WRITER_ID=${1:-0}
 BATCH_SIZE=${2:-64}
 
-tegrastats --start --interval 1000 --logfile data/QMNIST/models-subject-out/$WRITER_ID/personalize-nano-gpu-$BATCH_SIZE.txt &
+tegrastats --start --logfile ./data/QMNIST/models-subject-out/$WRITER_ID/personalize-nano-gpu-$BATCH_SIZE.txt
 
-time python mnist/2_personalize.py --log-interval=1 --writer_id=$WRITER_ID --batch_size=$BATCH_SIZE |& tee data/QMNIST/models-subject-out/$WRITER_ID/personalize-nano-gpu-$BATCH_SIZE.log
+time python mnist/2_personalize.py --log-interval=1 --writer_id=$WRITER_ID --batch_size=$BATCH_SIZE
 
 tegrastats --stop
---
+++ mnist/4_report_nano.py
@@ -0,0 +1,79 @@
+import os
+import torch
+import argparse
+import re
+import statistics
+import matplotlib.pyplot as plt
+
+from collections import defaultdict
+
+from edgify.utils import WriterQMNIST
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Report Stats from Training on Jetson Nano')
+    parser.add_argument('--file', type=str, default='train',
+                        help='Choose to filter the train or the test datasets')
+    
+    
+    args = parser.parse_args()
+    gpu_utils = []
+    cpu_utils = []
+    gpu_power = []
+    cpu_power = []
+    gpu_energy = 0
+    cpu_energy = 0
+    with open(args.file, 'r') as f:
+        for line in f:
+            match = re.search('GR3D_FREQ (?P<gpu_util>[0-9]+)\%' ,line)
+            gpu_utils.append(float(match.group('gpu_util')))
+
+            match = re.search('CPU \[(?P<cpu1_util>[0-9]+)\%\@[0-9]+,(?P<cpu2_util>[0-9]+)\%\@[0-9]+,off,off\] ' ,line)
+            cpu_utils.append( (float(match.group('cpu1_util')) + float(match.group('cpu2_util'))) / 2 )
+
+            match = re.search('POM_5V_GPU (?P<gpu_power>[0-9]+)\/[0-9]+', line)
+            gpu_power.append(float(match.group('gpu_power')))
+            gpu_energy += float(match.group('gpu_power'))
+
+            match = re.search('POM_5V_CPU (?P<cpu_power>[0-9]+)\/[0-9]+', line)
+            cpu_power.append(float(match.group('cpu_power')))
+            cpu_energy += float(match.group('cpu_power'))
+
+    print('Average GPU Util (%): ', statistics.mean(gpu_utils))
+    print('Average CPU Util (%): ', statistics.mean(cpu_utils))
+    
+    print('Average GPU Power (mW): ', statistics.mean(gpu_power))
+    print('Average CPU Power (mW): ', statistics.mean(cpu_power))
+
+    print('GPU Energy (J): ', gpu_energy / 1000.0)
+    print('CPU Energy (J): ', cpu_energy / 1000.0)
+
+    plt.plot(gpu_utils, color='red')
+    plt.xlabel('Time (s)')
+    plt.ylabel('GPU Utilization (%)', color='red')
+    
+    ax2 = plt.twinx()
+    ax2.plot(cpu_utils, color='blue')
+    ax2.set_ylabel('CPU Utilization (%)', color='blue')
+
+    plt.xlim([0, len(gpu_utils)])
+    plt.title('Batch Size: {}'.format(512))
+    plt.show()
+
+    plt.clf()
+
+    plt.plot(gpu_power, color='red')
+    plt.xlabel('Time (s)')
+    plt.ylabel('GPU Power (mW)', color='red')
+    
+    ax2 = plt.twinx()
+    ax2.plot(cpu_power, color='blue')
+    ax2.set_ylabel('CPU Power (mW)', color='blue')
+
+    plt.xlim([0, len(cpu_power)])
+    plt.ylim([0, max(cpu_power) + 50])
+    plt.title('Batch Size: {}'.format(512))
+    plt.show()
+    
+
+    
+    
