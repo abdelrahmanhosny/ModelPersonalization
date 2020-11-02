fe896b26cc8f23635c3f99e2f0c0487428a98adf
Abdelrahman <abdelrahman@brown.edu>
2020-11-01 20:06:09 -0500

    update nano stats

--- mnist/2_personalize_gpu_nano.sh
+++ mnist/2_personalize_gpu_nano.sh
@@ -3,13 +3,8 @@
 WRITER_ID=${1:-0}
 BATCH_SIZE=${2:-64}
 
-tegrastats --interval 1 --logfile data/QMNIST/models-subject-out/$WRITER_ID/personalize-nano-gpu-$BATCH_SIZE.txt &
-pid=$!
-
-sleep 5        # used to report idle power when util is zero
+tegrastats --start --interval 1000 --logfile data/QMNIST/models-subject-out/$WRITER_ID/personalize-nano-gpu-$BATCH_SIZE.txt &
 
 time python mnist/2_personalize.py --log-interval=1 --writer_id=$WRITER_ID --batch_size=$BATCH_SIZE |& tee data/QMNIST/models-subject-out/$WRITER_ID/personalize-nano-gpu-$BATCH_SIZE.log
 
-sleep 5        # used to report idle power when util is zero
-
-kill $pid
+tegrastats --stop
